34a609c4648eef98027bd973a5189dbd39720b14
Thiago Lobato <thiagolobato@outlook.com>
2025-03-25 08:39:22 -0300

    save data locally added

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,12 @@ listContainer.addEventListener("click", function(e){
     }
 }, false);
 
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
