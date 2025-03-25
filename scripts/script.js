7ca57cf1f8face4fbfc6e5fb61208f6a24356064
Thiago Lobato <thiagolobato@outlook.com>
2025-03-25 08:37:15 -0300

    check system & remove item script

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,15 @@ function addTask(){
     inputBox.value = "";
     saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
